31b485b0cdae7084ebb5f5c01634c8a9ff48c45c
Aryan Raj <aryan.raj@infostride.com>
2021-10-26 05:13:31 +0530

    Added registarion file with any validation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import Db from "./DataBase";
 import {indexName,docType} from './config'
+import Registraion from "./userAuthentication";
 function App() {
   const [name, setName] = useState("");
  
-  var payload;
-  payload = {
-    user: "elastic",
-    post_date: "2010-01-15T01:46:38",
-    message: "any message from the user",
-  };
+  
   Db.ping(
     {
       requestTimeout: Infinity,
@@ -25,58 +19,33 @@ function App() {
     }
   );
  
-  Db
-    .index({
-      index: indexName,
-      type: docType,
-      refresh: true,
-      body: payload,
-    })
-    .then(
-      function (resp) {
-        console.log("index is created ", resp);
-      },
-      function (err) {
-        console.log(err.message);
-      }
-    );
+ 
 
-  const getElasticSearchData = () => {
-    var that = this;
-    Db
-      .search({
-        index: indexName,
-        type: docType,
-        // refresh: true,
-        // body: payload
-      })
-      .then(
-        function (resp) {
-          console.log(resp.hits.hits);
-          that.setState({
-            dataList: resp.hits.hits,
-          });
-        },
-        function (err) {
-          console.log(err.message);
-        }
-      );
-  };
+  // const getElasticSearchData = () => {
+  //   var that = this;
+  //   Db
+  //     .search({
+  //       index: indexName,
+  //       type: docType,
+  //       // refresh: true,
+  //       // body: payload
+  //     })
+  //     .then(
+  //       function (resp) {
+  //         console.log(resp.hits.hits);
+  //         that.setState({
+  //           dataList: resp.hits.hits,
+  //         });
+  //       },
+  //       function (err) {
+  //         console.log(err.message);
+  //       }
+  //     );
+  // };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <input
-          value={name}
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
-        ></input>
-      </header>
+      <Registraion /> 
     </div>
   );
 }
--- src/DataBase/Db.js
+++ src/DataBase/Db.js
@@ -1,5 +1,4 @@
 import elasticsearch from 'elasticsearch'
-
 var Db = new elasticsearch.Client({
     host: 'http://localhost:9200/',
     requestTimeout: Infinity, // Tested
